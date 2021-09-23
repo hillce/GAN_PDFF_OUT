5daf502ebe7bf48e1e49ea932931c54ce3d421e6
hillce <charles.hill@dtc.ox.ac.uk>
2021-09-23 16:07:18 +0100

    reasons

--- genMetricsNFig.py
+++ genMetricsNFig.py
@@ -5,16 +5,31 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 fileDir = "./outputMAE/"
-maeList = [os.path.join(fileDir,x) for x in os.listdir(fileDir)]
+dataDir = "/home/chill/20254_2_0_NPY/"
+badEids = np.load("BadEids.npy")
+goodEids = np.load("GoodEids.npy")
 
+maeList = [x for x in os.listdir(fileDir) if x[:7] in badEids]
+
+plt.rcParams["figure.figsize"] = [6.4*4,4.8*4]
 for mae in maeList:
-    arr = np.load(mae)
+    arr = np.load(os.path.join(fileDir,mae))
+
+    imgs = np.load(os.path.join(dataDir,mae))
 
     fig,ax = plt.subplots(1,arr.shape[0])
     for i in range(arr.shape[0]):
-        im = ax[i].imshow(arr[i,:,:],cmap="plasma",vmax=3)
+        im = ax[i].imshow(arr[i,:,:],cmap="plasma",vmax=5)
         ax[i].axis("off")
         fig.colorbar(im,ax=ax[i],shrink=0.17)
 
-    plt.savefig("./tempFig.png")
-    
+    plt.savefig("./{}_bad.png".format(mae[:-4]))
+    plt.close("all")
+
+    fig,ax = plt.subplots(1,arr.shape[0])
+    for i in range(arr.shape[0]):
+        im = ax[i].imshow(imgs[i,:,:],cmap="gray")
+        ax[i].axis("off")
+        fig.colorbar(im,ax=ax[i],shrink=0.17)
+
+    plt.savefig("./{}_bad_img.png".format(mae[:-4]))
